6faf1dc6d7d2713853a796d942a737b55a213010
sembi <sembiyan.ta@gmail.com>
2021-07-24 20:05:53 +0530

    Update Exc1.py

--- Exc1.py
+++ Exc1.py
@@ -3,5 +3,7 @@ values = input("Enter comma separated values: ")
 list = values.split(",")
 tuple = tuple(list)
 
+#this is a forked branch
+
 print("list: {}".format(list))
 print("tuple: {}".format(tuple))
